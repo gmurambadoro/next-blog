9e545c069f511a0b009906982dd049dcfcdd175f
Gavin Murambadoro <developer.gavin@gmail.com>
2021-04-15 16:22:34 +0200

    Remove ts-ignore from higher up

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,6 @@ import Providers from "next-auth/providers";
 import {APP_ENV, APP_SECRET, DATABASE_URL, GITHUB_ID, GITHUB_SECRET} from "../../../config";
 import {JWT} from "next-auth/jwt";
 
-// @ts-ignore
-// @ts-ignore
 export default NextAuth({
     providers: [
         Providers.GitHub({
